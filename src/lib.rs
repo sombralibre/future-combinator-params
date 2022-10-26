3fc135563ef800f96edcabd4571cbebf9b6dc250
Alejandro Llanes <sombra.libre@gmail.com>
2022-10-26 17:26:42 -0500

    fix bounds

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,25 @@ use futures::Future;
 use std::fmt::Debug;
 use std::{boxed::Box, pin::Pin};
 
-pub fn combine_param<R: Sized + 'static, T: Sized + 'static, E: Sized + 'static + Debug>(
+pub fn combine_param<R, T, E>(
     t: T,
-) -> impl FnOnce(Result<R, E>) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>> {
-    |r: Result<R, E>| Box::pin(async { Ok((r.unwrap(), t)) })
+) -> impl FnOnce(Result<R, E>) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>>
+where
+    R: Sized + 'static + Sync + Send,
+    T: Sized + 'static + Sync + Send,
+    E: Sized + 'static + Debug + Sync + Send,
+{
+    |r: Result<R, E>| Box::pin(async { Ok((r?, t)) })
 }
 
-pub fn combine_flat_param<R: Sized + 'static, T: Sized + 'static, E: Sized + 'static + Debug>(
+pub fn combine_flat_param<R, T, E>(
     t: T,
-) -> impl FnOnce(R) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>> {
+) -> impl FnOnce(R) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>>
+where
+    R: Sized + 'static + Sync + Send,
+    T: Sized + 'static + Sync + Send,
+    E: Sized + 'static + Debug + Sync + Send,
+{
     |r: R| Box::pin(async { Ok((r, t)) })
 }
 

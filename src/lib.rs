ac59164867a34d3593b35c923e1ca66c4e1f0ce7
Alejandro Llanes <sombra.libre@gmail.com>
2022-10-26 17:02:19 -0500

    fix error generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use futures::Future;
+use std::fmt::Debug;
 use std::{boxed::Box, pin::Pin};
 
-pub fn combine_param<R: Sized + 'static, T: Sized + 'static>(
+pub fn combine_param<R: Sized + 'static, T: Sized + 'static, E: Sized + 'static + Debug>(
     t: T,
-) -> impl FnOnce(Result<R, ()>) -> Pin<Box<dyn Future<Output = Result<(R, T), ()>>>> {
-    |r: Result<R, ()>| Box::pin(async { Ok((r.unwrap(), t)) })
+) -> impl FnOnce(Result<R, E>) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>> {
+    |r: Result<R, E>| Box::pin(async { Ok((r.unwrap(), t)) })
 }
 
-pub fn combine_flat_param<R: Sized + 'static, T: Sized + 'static>(
+pub fn combine_flat_param<R: Sized + 'static, T: Sized + 'static, E: Sized + 'static + Debug>(
     t: T,
-) -> impl FnOnce(R) -> Pin<Box<dyn Future<Output = Result<(R, T), ()>>>> {
+) -> impl FnOnce(R) -> Pin<Box<dyn Future<Output = Result<(R, T), E>>>> {
     |r: R| Box::pin(async { Ok((r, t)) })
 }
 
@@ -19,8 +20,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        
+
         //assert_eq!(result, 4);
     }
 } */
-
